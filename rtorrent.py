e08eb8bf01b620284427561e79b16264c5bb9a6e
Merlijn Wajer <merlijn@wizzup.org>
2011-01-24 15:17:32 +0100

    Models: Add initial stubs.

---
+++ rtorrent.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+"""
+RTorrent class.
+
+Supported protocols:
+    -   HTTP
+To support:
+    -   SCGI (with xmlrpc2scgi.py from rtorrent wiki)
+"""
+
+import xmlrpclib
+
+class RTorrent(object):
+    """
+
+    """
+
+    def __init__(self, host, port):
+        pass
---
+++ torrent.py
@@ -0,0 +1,4 @@
+
+class Torrent(object):
+    """
+    """
