d8c3dd600f69c7f519ecb71648e32e28fac76253
Merlijn Wajer <merlijn@wizzup.org>
2011-01-24 17:14:14 +0100

    RTorrent:   Add get/set upload rate.

--- rtorrent.py
+++ rtorrent.py
@@ -15,5 +15,21 @@ class RTorrent(object):
 
     """
 
-    def __init__(self, host, port):
-        pass
+    def __init__(self, host, port=80):
+        # No ending '/' !
+        self.s = xmlrpclib.ServerProxy('http://%s:%i' % (host, port))
+
+    def get_upload_rate(self):
+        return self.s.get_upload_rate()
+
+    def set_upload_rate(self, rate):
+        # If invalid, raise ValueError.
+        rate = int(rate)
+        return self.s.set_upload_rate(rate)
+
+if __name__ == '__main__':
+    r = RTorrent('sheeva')
+
+    print r.get_upload_rate()
+
+
