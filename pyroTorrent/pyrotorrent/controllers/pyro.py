51eb28838015af1352aaa1013510b90fd03d0267
Merlijn Wajer <merlijn@wizzup.org>
2011-01-27 20:47:50 +0100

    Pylons: Updated pyro controller. Moved models.
    Moved RTorrent and Torrent to models/
    Created the templates/ folder and added download_list.jinja2.
    Pyro controller currently lists hashes of all downloads.

--- pyroTorrent/pyrotorrent/controllers/pyro.py
+++ pyroTorrent/pyrotorrent/controllers/pyro.py
@@ -4,6 +4,7 @@ from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
 from pyrotorrent.lib.base import BaseController, render
+from pyrotorrent.model.rtorrent import RTorrent
 
 log = logging.getLogger(__name__)
 
@@ -13,4 +14,10 @@ class PyroController(BaseController):
         # Return a rendered template
         #return render('/pyro.mako')
         # or, return a string
+
+        r = RTorrent('sheeva')
+
+        c.download_titles = r.get_download_list()
+
+        return render('/download_list.jinja2')
         return 'Welcome to pyroTorrent'
