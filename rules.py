3003c19fe3221dbf3bfbfebefd14128f89a1de37
Merlijn Wajer <merlijn@wizzup.org>
2011-05-21 21:52:07 +0200

    pyroTorrent: Fix rules.

--- rules.py
+++ rules.py
@@ -4,4 +4,4 @@ wt.add_rule(re.compile('^%s/torrent/[0-9,A-Z]{32}/?' % BASE_URL),
         torrent_info_page, ['torrent_hash'])
 
 # This should be the last rule.
-wt.add_rule(re.compile('^%s/$' % BASE_URL), main_page, [])
+wt.add_rule(re.compile('^%s/?$' % BASE_URL), main_page, [])
