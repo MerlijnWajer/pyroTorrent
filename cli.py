4f4bef0793d7d7bd60faa775423753dff20f17b0
Merlijn Wajer <merlijn@wizzup.org>
2011-07-27 12:41:21 +0200

    pyroTorrent: Updated cli.py and TODO.
    Use ``from cli import *'' to use it.

--- cli.py
+++ cli.py
@@ -4,23 +4,10 @@ from model.rtorrent import RTorrent
 
 from lib.torrentrequester import TorrentRequester
 
-if __name__ == '__main__':
-    r = RTorrent()
-    print r.get_upload_rate()
+r = RTorrent()
 
-    #torrents = r.get_download_list('')
-    torrents = TorrentRequester('')
-    torrents.get_hash().get_name()
+print '''Welcome to the pyroTorrent CLI interface.
+We have created a RTorrent() object called 'r'.
 
-    a = torrents.all()
-    print a
-
-    torrenthash = a[0].get_hash
-
-    t = Torrent(torrenthash)
-
-    q = t.query()
-
-    q.get_name().get_size_bytes().get_bytes_left().get_loaded_file()
-
-    print q.all()[0]
+Use the help() commands to find out how to use the client interface;
+help(RTorrent), help(Torrent) might be helpful'''
