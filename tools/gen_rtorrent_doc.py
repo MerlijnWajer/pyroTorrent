4a75898227e6fc4ca6b42ce09ff57087071284fd
Merlijn Wajer <merlijn@wizzup.org>
2011-07-29 13:08:10 +0200

    Tools: Update gen_rtorrent_doc.py

--- tools/gen_rtorrent_doc.py
+++ tools/gen_rtorrent_doc.py
@@ -4,9 +4,9 @@ sys.path.insert(0, '..')
 import model.rtorrent as rtorrent
 import model.torrent as torrent
 import model.peer as peer
+import model.torrentfile as torrentfile
 
 
-for d in (rtorrent, torrent, peer):
+for d in (rtorrent, torrent, peer, torrentfile):
     for x, y in d._rpc_methods.iteritems():
         print y[0], y[1]
-        #print y[0], '\n', y[1]
