3414c143eec1a5e01ca87f445046c85d2c537292
Merlijn Wajer <merlijn@wizzup.org>
2011-07-29 18:13:38 +0200

    pyroTorrent: Remove pyroTorrent_old.

--- pyroTorrent_old/pyrotorrent/tests/functional/test_pyro.py
+++
@@ -1,7 +0,0 @@
-from pyrotorrent.tests import *
-
-class TestPyroController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='pyro', action='index'))
-        # Test response...
--- pyroTorrent_old/pyrotorrent/tests/functional/test_torrent.py
+++
@@ -1,7 +0,0 @@
-from pyrotorrent.tests import *
-
-class TestTorrentController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='torrent', action='index'))
-        # Test response...
