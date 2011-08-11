804de075eafcafbde74b45cb8dc2d399757399d8
Merlijn Wajer <merlijn@wizzup.org>
2011-08-11 14:37:02 +0200

    PyroTorrent: Fix test.py

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 from model.rtorrent import RTorrent
 import socket
 
-r = RTorrent()
+from config import rtorrent_config
+from lib.config_parser import parse_config_part, RTorrentConfigException
 
-try:
-    print 'libTorrent version:', r.get_libtorrent_version()
-except socket.error, e:
-    print 'Failed to connect to libTorrent:', str(e)
+targets = []
+for x in rtorrent_config:
+    try:
+        info = parse_config_part(rtorrent_config[x], x)
+    except RTorrentConfigException, e:
+        print 'Invalid config: ', e
+        sys.exit(1)
+
+    targets.append(info)
+
+for x in targets:
+    r = RTorrent(x)
+
+    try:
+        print 'libTorrent version:', r.get_libtorrent_version()
+    except socket.error, e:
+        print 'Failed to connect to libTorrent:', str(e)
