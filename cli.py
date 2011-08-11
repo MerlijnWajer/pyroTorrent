a9a0186a20015f64a6475b98e6fb42d6efc74628
Merlijn Wajer <merlijn@wizzup.org>
2011-08-11 16:11:30 +0200

    Cli.py now works with multiple targets.

--- cli.py
+++ cli.py
@@ -6,10 +6,29 @@ from model.peer import Peer
 from lib.torrentrequester import TorrentRequester
 from lib.peerrequester import PeerRequester
 
-r = RTorrent()
+from config import rtorrent_config
+from lib.config_parser import parse_config_part, RTorrentConfigException
+
+targets = []
+for x in rtorrent_config:
+    try:
+        info = parse_config_part(rtorrent_config[x], x)
+    except RTorrentConfigException, e:
+        print 'Invalid config: ', e
+        sys.exit(1)
+
+    targets.append(info)
+
+if len(x) == 0:
+    print 'No targets'
+    sys.exit(1)
+
+r = RTorrent(targets[0])
 
 print '''Welcome to the pyroTorrent CLI interface.
 We have created a RTorrent() object called 'r'.
 
 Use the help() commands to find out how to use the client interface;
 help(RTorrent), help(Torrent) might be helpful'''
+
+print 'RTorrent object using target: ', targets[0]
